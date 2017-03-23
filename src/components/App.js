0c8085e727d5c6fa3c8917b6d6ebde6102690f53
Brian Bycynski <high5bri@hotmail.com>
2017-03-23 10:51:50 -0400

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 class App extends Component {
   render () {
     return <div>
+      <h1>Resize the window</h1>
       <div className='grid'>
         <div>1</div>
         <div>2</div>
@@ -12,6 +13,22 @@ class App extends Component {
         <div>6</div>
         <div>7</div>
         <div>8</div>
+      </div>
+      <div className='grid1'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+      </div>
+      <div className='grid2'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
       </div>
     </div>
   }
